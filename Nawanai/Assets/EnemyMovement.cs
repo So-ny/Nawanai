3eb49d8b1b8fe14e8ee977a69cbce2a2095e17dc
So-ny <syonchau@gmail.com>
2023-05-16 15:19:21 -0700

    enemy move

---
+++ Nawanai/Assets/EnemyMovement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed;
+
+    public bool MoveRight;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(MoveRight)
+        {
+            transform.Translate(2 * Time.deltaTime * speed, 0,0);
+            transform.localScale = new Vector2(1,1);
+        }
+        else
+        {
+            transform.Translate(-2 * Time.deltaTime * speed, 0,0);
+            transform.localScale = new Vector2(-1,1);
+
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.CompareTag("Turn"))
+        {
+            if(MoveRight)
+            {
+                MoveRight = false;
+            }
+            else 
+            {
+                MoveRight = true;
+            }
+            Debug.Log("hit");
+        }
+    }
+}
