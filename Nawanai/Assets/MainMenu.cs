b6955e0c9f7880044922d052e9561318ce459717
So-ny <syonchau@gmail.com>
2023-05-18 11:57:03 -0700

    Finished Main Menu and got button to work
    Not sure how I should make the main menu look

--- Nawanai/Assets/MainMenu.cs
+++ Nawanai/Assets/MainMenu.cs
@@ -1,8 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void 
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 }
